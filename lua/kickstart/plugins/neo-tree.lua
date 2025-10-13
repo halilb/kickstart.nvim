690b49258788726b50c1427e0fbcb884dfae2a7f
Halil Bilir <hbilir@gmail.com>
2025-10-13 11:14:55 +0300

    Update dependencies.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -3,13 +3,15 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  -- Pinning the version because of this bug:
+  -- https://github.com/nvim-neo-tree/neo-tree.nvim/pull/1896#issuecomment-3395722326
+  version = '3.36.2',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
